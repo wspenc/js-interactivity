9e7d2e9747ca4502645422ed47ce8dfbd0ccd3c0
Will Spencer <willspencer@me.com>
2022-10-12 16:10:42 -0600

    Movie list working

---
+++ index.js
@@ -0,0 +1,48 @@
+//run addMovie function once submit or "add" is clicked 
+//chained due to the form element
+document.querySelector('form').addEventListener('submit', addMovie);
+//Will be used to display message after action takes place (deleting a movie)
+const message = document.querySelector('#message');
+
+//add movie once add is clicked
+function addMovie(event) {
+event.preventDefault()
+let inputField = document.querySelector('input');
+
+//Creates an element that'll store movie in list, as well as a new span element (keeps it inline)
+const movie = document.createElement('li')
+const movieTitle = document.createElement('span');
+
+//New span that prints title that was inputted
+movieTitle.textContent = inputField.value;
+movieTitle.addEventListener('click', crossOffMovie)
+
+//Adds a new node "Movie Title" to DOM
+movie.appendChild(movieTitle);
+
+//delete button
+const deleteBtn = document.createElement('button');
+deleteBtn.textContent = 'X'
+deleteBtn.addEventListener("click", deleteMovie);
+//Replace the parent once "X" is clicked
+movie.appendChild(deleteBtn);
+
+//Will create un-ordered list once movie title is found
+const list = document.querySelector('ul');
+list.appendChild(movie);
+inputField.value = ' '
+} 
+
+//delete function
+function deleteMovie(event) {
+    message.textContent = 'Movie Deleted'
+    event.target.parentNode.remove();
+ }
+
+ //Create strike through that will "add a movie back" or "watched"
+function crossOffMovie(event)  {
+    event.target.classList.toggle('checked')
+        if(event.target.classList.contains('checked')){
+            message.textContent = 'Movie added back!'
+        }
+}
